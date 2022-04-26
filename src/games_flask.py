377c4b13ca121cca5912d0c5c9410b8d637b61eb
Juri Kivimae <kivimae@gmail.com>
2022-04-26 14:43:02 -0400

    Updated some files

--- src/games_flask.py
+++ src/games_flask.py
@@ -1,33 +1,32 @@
 ''' Flask app that takes in a game title and returns the top 10 similar games '''
 
 # import libraries
-from flask import Flask, Response, request, render_template
-import json
-import pandas as pd
-from wtforms import StringField, Form
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import pickle
 
 app = Flask(__name__)
 
-gamesinfo_df = pd.read_csv('data/steam_gameinfo.csv')
+# Flask-WTF requires an encryption key - the string can be anything
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+
+# Flask-Bootstrap requires this line
+Bootstrap(app)
+
+gamesinfo_df = pd.read_csv('../data/steam_gameinfo.csv')
 games = gamesinfo_df['name'].tolist()
 
-# load pickle model
-knn_model = pickle.load(open('models/knn_model.pkl', 'rb'))
+# load pickle knn model
+knn = pickle.load(open('../models/knn_model.pkl', 'rb'))
 
-games =['aa', 'bb']
+# load pickle cosine similarity model
 
-class SearchForm(Form):
-    autocomp = StringField('Enter Game', id='game_autocomplete')
-
-@app.route('/_autocomplete', methods=['GET'])
-def autocomplete():
-    return Response(json.dumps(games), mimetype='application/json')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = SearchForm(request.form)
-    return render_template("index.html", form=form)
+class NameForm(FlaskForm):
+    name = StringField('Which actor is your favorite?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run(debug=True)
