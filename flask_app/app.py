46a97283682bdb0d2981b4a719da691891397c43
Juri Kivimae <kivimae@gmail.com>
2023-01-17 16:11:14 +0200

    flask app refactoring, improvements, wip

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,12 @@
 ''' Flask app that takes in a game title and returns the top 10 similar games '''
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from utils import combined_recom
+from utils.utils import combined_recom
 
 import os
 import pandas as pd
@@ -14,7 +14,7 @@ from os import path
 
 SITE_ROOT = path.realpath(path.dirname(__file__))
 # get games info
-gamesinfo_df = pd.read_csv(path.join(SITE_ROOT, '../data/steam_gameinfo.csv'))
+games_df = pd.read_csv(path.join(SITE_ROOT, '../data/steam_app_metadata.csv'))
 
 app = Flask(__name__)
 
@@ -31,16 +31,18 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 # route for home page
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
+    # if request
+    name = request.args.get('name')
     form = NameForm()
     message = ""
-    if form.validate_on_submit():
-        title = form.name.data
-        if title in gamesinfo_df['name'].values:
-            collabr, contentr = combined_recom(title)
+    if name != None:
+        title = name
+        collabr, contentr = combined_recom(title)
+        if collabr:
             message = "Here are the top 10 recommendations for " + title
             return render_template('index.html', form=form, message=message, collabr=collabr, contentr=contentr)
         else:
--- flask_app/utils/utils.py
+++ flask_app/utils/utils.py
@@ -5,7 +5,7 @@ from os import path
 SITE_ROOT = path.realpath(path.dirname(__file__))
 
 # get games info
-gamesinfo_df = pd.read_csv(path.join(SITE_ROOT, '../../data/steam_gameinfo.csv'))
+games_df = pd.read_csv(path.join(SITE_ROOT, '../../data/steam_app_metadata.csv'))
 
 # load pickle knn model
 knn = pickle.load(open(path.join(SITE_ROOT, '../../models/knn_model.pkl'), 'rb'))
@@ -17,18 +17,18 @@ trainset = pickle.load(open(path.join(SITE_ROOT, '../../models/trainset.pkl'), '
 cosine_sim = pickle.load(open(path.join(SITE_ROOT, '../../models/cosine_sim.pkl'), 'rb'))
 
 # Construct a reverse map of indices and game titles
-indices = pd.Series(gamesinfo_df.index, index=gamesinfo_df['name'])
+indices = pd.Series(games_df.index, index=games_df['name'])
 
 # function that takes game title and returns the inner id
 def get_appid(title):
-    steam_id = gamesinfo_df[gamesinfo_df['name'] == title].iloc[0]['appid']
+    steam_id = games_df[games_df['name'] == title].iloc[0]['appid']
     inner_id = trainset.to_inner_iid(steam_id)
     return inner_id
 
 # function that takes innerid and returns the game title
 def get_title(appid):
     steam_id = trainset.to_raw_iid(appid)
-    title = gamesinfo_df[gamesinfo_df['appid'] == steam_id].iloc[0]['name']
+    title = games_df[games_df['appid'] == steam_id].iloc[0]['name']
     return title
 
 # function that takes a game title and returns 10 nearest neighbours
@@ -74,13 +74,16 @@ def recommend_content(title, cosine_sim = cosine_sim):
     game_indices = [i[0] for i in sim_scores]
     
     # return the titles
-    return gamesinfo_df['name'].iloc[game_indices].tolist()
+    return games_df['name'].iloc[game_indices].tolist()
 
 # function that takes a game title and returns top 10 for both
 def combined_recom(title):
-    # get recommended games from knn
-    knn_recom = recommend_knn(title)
-    # get recommended games from content based
-    cont_recom = recommend_content(title)
-    
-    return knn_recom, cont_recom
+    if title not in games_df['name'].values:
+        return None
+    else:
+        # get recommended games from knn
+        knn_recom = recommend_knn(title)
+        # get recommended games from content based
+        cont_recom = recommend_content(title)
+        
+        return knn_recom, cont_recom
