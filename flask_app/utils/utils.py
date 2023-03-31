96bb14d9d799206f64c888cf533731a5c245c369
Juri Kivimae <kivimae@gmail.com>
2023-03-31 15:13:07 +0300

    updated loading folders in utils

--- flask_app/utils/utils.py
+++ flask_app/utils/utils.py
@@ -5,9 +5,9 @@ from os import path
 SITE_ROOT = path.realpath(path.dirname(__file__))
 
 # get games info
-games_df = pd.read_csv(path.join(SITE_ROOT, '../data/steam_metadata_flask.csv'))
+games_df = pd.read_csv(path.join(SITE_ROOT, 'steam_metadata_flask.csv'))
 
-weighted_sim = pickle.load(open(path.join(SITE_ROOT, '../models/weighted_sim_compressed.pkl'), 'rb'))
+weighted_sim = pickle.load(open(path.join(SITE_ROOT, 'weighted_sim_compressed.pkl'), 'rb'))
 
 # Construct a reverse map of indices and game titles
 indices = pd.Series(games_df.index, index=games_df['name'])
@@ -42,7 +42,6 @@ def recommend_content(title, sim_matrix = weighted_sim):
     
     # sort scores based on similarity
     sorted_sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    print(len(sorted_sim_scores))
     
     # create a list of dictionaries with title, description and score
     content_similar_games = [{'title': indices.index[i[0]],
